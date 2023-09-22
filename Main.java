be186c991ff395c97de2f6608f58dc20fb06aebc
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-09-23 01:45:58 +0300

    #3 – Базовые концепции языка. Вывод информации

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("!");
+    }
+}
